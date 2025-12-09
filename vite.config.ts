c79e5c9ae1a8d55ec2b60090248d9bbcec5a3c93
Phát Tài Tăng Nguyễn <92193419+tykyfatkie@users.noreply.github.com>
2025-12-09 22:12:00 +0700

    Đã thêm "client.tissuex.me" vào danh sách cho phép

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
     port: 3000,
-    allowedHosts: ["client.orchid-lab.systems"],
+    allowedHosts: ["client.orchid-lab.systems", "client.tissuex.me"],
     strictPort: true,
     proxy: {
       "/api": {
